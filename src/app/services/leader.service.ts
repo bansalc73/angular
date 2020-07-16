1272b08b83a820e581af37925feed0efe9b38121
Chirag <bansalc73@gmail.com>
2020-07-16 12:09:01 +0530

    Promise P2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Leader,LEADERS } from '../shared/leader';
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,21 @@ export class LeaderService {
 
   constructor() { }
   getLeaders() : Promise<Leader[]>{
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS),2000)
+    });
   }
 
   getLeader(id:string) :Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id===id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id===id))[0]),2000)
+    });
   }
 
 
   getFeaturedLeader() :Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => (leader.featured))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.featured))[0]),2000)
+    });
   }
 }
